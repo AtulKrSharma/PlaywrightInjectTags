5204adac1058aca697037f96f872e151dc249e32
Atul Sharma <49794348+AtulKrSharma@users.noreply.github.com>
2024-10-24 00:24:14 -0400

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? undefined : 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: [["html"], ["blob"]],
-  reporter: process.env.CI ? "blob" : "html",
+  reporter: process.env.CI ? "html" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
